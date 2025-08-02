d0af9ded26c40a2ea6e881cd8221fdb46a6c2089
Raphael Rodrigues <73262792+raphadvlp@users.noreply.github.com>
2025-08-02 18:19:12 -0300

    ajuste altura container cards turma

--- src/pages/Turmas/index.jsx
+++ src/pages/Turmas/index.jsx
@@ -10,8 +10,9 @@ function Turmas() {
             <div className="flex flex-col p-5">
                 <div className="w-full flex justify-center items-center text-3xl font-bold mb-15">
                     <h1>Turmas</h1>
+                    
                 </div>
-                <div className="max-h-96 overflow-y-auto flex flex-col gap-1 pr-3">
+                <div className="h-96 overflow-y-auto flex flex-col gap-1 pr-3">
 
                     <div className="flex bg-white justify-between items-center p-2 rounded-md">
                         <div className="text-black">
