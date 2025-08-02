e4f00151dab3488dda5ddd9d644db2629a5ff92a
Raphael Rodrigues <73262792+raphadvlp@users.noreply.github.com>
2025-08-02 12:01:40 -0300

    teste margens

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,31 +2,31 @@
 
 function Login() {
     return (
-            <div className="w-screen h-screen pl-5 pr-5 bg-gray-900 text-white flex flex-col justify-center">
+            <div className="w-screen h-screen pl-5 pr-5 bg-gray-900 text-white flex flex-col justify-center overflow-hidden">
                 <header className="flex flex-col justify-center items-center">
                     <div>
                         <img src="logo.png" alt="Logo Igreja" />
                     </div>
                     <div>
-                        <h1 className="text-3xl font-bold m-10">Escola Bíblica Dominical</h1>
+                        <h1 className="text-3xl font-bold py-6 text-center">Escola Bíblica Dominical</h1>
                     </div>
                 </header>
-                <form className="flex flex-col  mb-8" action="">
+                <form className="flex flex-col flex-grow justify-center" action="">
                     <div className="flex flex-col">
                         <label htmlFor="usuarioField">Usuário</label>
-                        <input className="bg-gray-700 h-12 rounded-md mb-5 p-3 outline-yellow-500 w-full text-lg" type="text" name="" id="usuarioField" placeholder='Insira seu usuário'/>
+                        <input className="bg-gray-700 h-12 rounded-md mb-5 p-3 outline-yellow-500 w-full text-lg" type="text" id="usuarioField" placeholder='Insira seu usuário'/>
                     </div>
                     
                     <div className="flex flex-col">
-                        <label htmlFor="usuarioField">Senha</label>
-                        <input className="bg-gray-700 h-12 rounded-md p-3 outline-yellow-500 w-full text-lg" type="text" name="" id="usuarioField" placeholder='Insira sua senha'/>
+                        <label htmlFor="senhaField">Senha</label>
+                        <input className="bg-gray-700 h-12 rounded-md p-3 outline-yellow-500 w-full text-lg" type="password" id="senhaField" placeholder='Insira sua senha'/>
                     </div>
 
-                    <div className="my-6">
+                    <div className="mt-6">
                         <button className="w-full p-2 rounded-md text-lg font-bold bg-amber-600" type="submit">Entrar</button>
                     </div>
                 </form>
-                <div className="w-full text-center">
+                <div className="w-full text-center mt-4">
                     <span>Assembléia de Deus São João de Meriti</span>
                 </div>
             </div>
