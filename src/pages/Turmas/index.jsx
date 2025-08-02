dad3a467cb09d3961de012a14ae422992dd8f892
Raphael Rodrigues <73262792+raphadvlp@users.noreply.github.com>
2025-08-02 18:09:30 -0300

    implantação pagina de turmas

--- src/pages/Turmas/index.jsx
+++ src/pages/Turmas/index.jsx
@@ -1,78 +1,180 @@
+import { Link } from 'react-router-dom';
 import NavBar from './../../components/NavBar/index';
+
 
 // Os cards tem que se tornar links
 
 function Turmas() {
     return (
         <>
-            <div className="">
-                <h1>Turmas</h1>
-                <div className="">
+            <div className="flex flex-col p-5">
+                <div className="w-full flex justify-center items-center text-3xl font-bold mb-15">
+                    <h1>Turmas</h1>
+                </div>
+                <div className="h-80 overflow-y-auto flex flex-col gap-1 pr-3">
 
-                    <div className="">
-                        <div className="">
+                    <div className="flex bg-white justify-between items-center p-2 rounded-md">
+                        <div className="text-black">
                             <span>Adolescentes</span>
                             <p>09:00h - 10:30h</p>
                         </div>
-                        <div className="">
+                        <div className=" flex gap-5">
                             <div className="">
-                                <img src="edit.svg" alt="" />
+                                <Link to="">
+                                    <img className='w-9' src="edit.svg" alt="" />
+                                </Link>
                             </div>
                             <div className="">
-                                <img src="delete.svg" alt="" />
+                                <Link to="">
+                                    <img className='w-9' src="delete.svg" alt="" />
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex bg-white justify-between items-center p-2 rounded-md gap-5">
+                        <div className="text-black">
+                            <span>Adolescentes</span>
+                            <p>09:00h - 10:30h</p>
+                        </div>
+                        <div className=" flex gap-5">
+                            <div className="">
+                                <Link to="">
+                                    <img className='w-9' src="edit.svg" alt="" />
+                                </Link>
+                            </div>
+                            <div className="">
+                                <Link to="">
+                                    <img className='w-9' src="delete.svg" alt="" />
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex bg-white justify-between items-center p-2 rounded-md gap-5">
+                        <div className="text-black">
+                            <span>Adolescentes</span>
+                            <p>09:00h - 10:30h</p>
+                        </div>
+                        <div className=" flex gap-5">
+                            <div className="">
+                                <Link to="">
+                                    <img className='w-9' src="edit.svg" alt="" />
+                                </Link>
+                            </div>
+                            <div className="">
+                                <Link to="">
+                                    <img className='w-9' src="delete.svg" alt="" />
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex bg-white justify-between items-center p-2 rounded-md gap-5">
+                        <div className="text-black">
+                            <span>Adolescentes</span>
+                            <p>09:00h - 10:30h</p>
+                        </div>
+                        <div className=" flex gap-5">
+                            <div className="">
+                                <Link to="">
+                                    <img className='w-9' src="edit.svg" alt="" />
+                                </Link>
+                            </div>
+                            <div className="">
+                                <Link to="">
+                                    <img className='w-9' src="delete.svg" alt="" />
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex bg-white justify-between items-center p-2 rounded-md gap-5">
+                        <div className="text-black">
+                            <span>Adolescentes</span>
+                            <p>09:00h - 10:30h</p>
+                        </div>
+                        <div className=" flex gap-5">
+                            <div className="">
+                                <Link to="">
+                                    <img className='w-9' src="edit.svg" alt="" />
+                                </Link>
+                            </div>
+                            <div className="">
+                                <Link to="">
+                                    <img className='w-9' src="delete.svg" alt="" />
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex bg-white justify-between items-center p-2 rounded-md gap-5">
+                        <div className="text-black">
+                            <span>Adolescentes</span>
+                            <p>09:00h - 10:30h</p>
+                        </div>
+                        <div className=" flex gap-5">
+                            <div className="">
+                                <Link to="">
+                                    <img className='w-9' src="edit.svg" alt="" />
+                                </Link>
+                            </div>
+                            <div className="">
+                                <Link to="">
+                                    <img className='w-9' src="delete.svg" alt="" />
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex bg-white justify-between items-center p-2 rounded-md gap-5">
+                        <div className="text-black">
+                            <span>Adolescentes</span>
+                            <p>09:00h - 10:30h</p>
+                        </div>
+                        <div className=" flex gap-5">
+                            <div className="">
+                                <Link to="">
+                                    <img className='w-9' src="edit.svg" alt="" />
+                                </Link>
+                            </div>
+                            <div className="">
+                                <Link to="">
+                                    <img className='w-9' src="delete.svg" alt="" />
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex bg-white justify-between items-center p-2 rounded-md gap-5">
+                        <div className="text-black">
+                            <span>Adolescentes</span>
+                            <p>09:00h - 10:30h</p>
+                        </div>
+                        <div className=" flex gap-5">
+                            <div className="">
+                                <Link to="">
+                                    <img className='w-9' src="edit.svg" alt="" />
+                                </Link>
+                            </div>
+                            <div className="">
+                                <Link to="">
+                                    <img className='w-9' src="delete.svg" alt="" />
+                                </Link>
                             </div>
                         </div>
                     </div>
                                         
-                    <div className="">
-                        <div className="">
-                            <span>Adolescentes</span>
-                            <p>09:00h - 10:30h</p>
-                        </div>
-                        <div className="">
-                            <div className="">
-                                <img src="edit.svg" alt="" />
-                            </div>
-                            <div className="">
-                                <img src="delete.svg" alt="" />
-                            </div>
-                        </div>
-                    </div>
-                                        
-                    <div className="">
-                        <div className="">
-                            <span>Adolescentes</span>
-                            <p>09:00h - 10:30h</p>
-                        </div>
-                        <div className="">
-                            <div className="">
-                                <img src="edit.svg" alt="" />
-                            </div>
-                            <div className="">
-                                <img src="delete.svg" alt="" />
-                            </div>
-                        </div>
-                    </div>
-                                        
-                    <div className="">
-                        <div className="">
-                            <span>Adolescentes</span>
-                            <p>09:00h - 10:30h</p>
-                        </div>
-                        <div className="">
-                            <div className="">
-                                <img src="edit.svg" alt="" />
-                            </div>
-                            <div className="">
-                                <img src="delete.svg" alt="" />
-                            </div>
-                        </div>
-                    </div>
+                    
                     
                 </div>
-                <button type="submit">Cadastrar</button>
+                <Link to="/cadastro/turmas" className='bg-amber-600 mt-5 h-10 rounded-md flex justify-center items-center'>
+                    <span>Cadastrar</span>
+                </Link>
             </div>
-            <NavBar />
+            <div className="fixed bottom-0 left-0 w-full">
+                <NavBar />
+            </div>
         </>
     )
 }
