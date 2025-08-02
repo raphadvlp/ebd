05b3dbfcb5117b6bd8fffb8535526f24ca4faa94
Raphael Rodrigues <73262792+raphadvlp@users.noreply.github.com>
2025-08-02 11:43:59 -0300

    correções de espaços

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,20 +3,23 @@
 function Login() {
     return (
             <div className="w-screen h-screen p-5 bg-gray-900 text-white">
-                <div className="h-full">
-                <header className="flex flex-col justify-center items-center gap-5">
-                    <img src="logo.png" alt="Logo Igreja" />
-                    <h1 className="text-3xl font-bold">Escola Bíblica Dominical</h1>
+                <header className="flex flex-col justify-center items-center">
+                    <div>
+                        <img src="logo.png" alt="Logo Igreja" />
+                    </div>
+                    <div>
+                        <h1 className="text-3xl font-bold m-10">Escola Bíblica Dominical</h1>
+                    </div>
                 </header>
-                <form className="flex flex-col gap-5 mt-5 mb-5" action="">
+                <form className="flex flex-col gap-5 mb-8" action="">
                     <div className="flex flex-col">
                         <label htmlFor="usuarioField">Usuário</label>
-                        <input className="bg-gray-700 h-11 rounded-md p-3 outline-yellow-500 w-full text-lg" type="text" name="" id="usuarioField" placeholder='Insira seu usuário'/>
+                        <input className="bg-gray-700 h-12 rounded-md p-3 outline-yellow-500 w-full text-lg" type="text" name="" id="usuarioField" placeholder='Insira seu usuário'/>
                     </div>
                     
                     <div className="flex flex-col">
                         <label htmlFor="usuarioField">Senha</label>
-                        <input className="bg-gray-700 h-11 rounded-md p-3 outline-yellow-500 w-full text-lg" type="text" name="" id="usuarioField" placeholder='Insira sua senha'/>
+                        <input className="bg-gray-700 h-12 rounded-md p-3 outline-yellow-500 w-full text-lg" type="text" name="" id="usuarioField" placeholder='Insira sua senha'/>
                     </div>
 
                     <div className="my-6">
@@ -27,7 +30,6 @@ function Login() {
                     <span>Assembléia de Deus São João de Meriti</span>
                 </div>
                 </div>
-            </div>
     )
 }
 
